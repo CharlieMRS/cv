18787536bbd3de281fa5584a2c411c2e0317b4e5
Charlie Meers <charlie.meers@lifestylepubs.com>
2020-04-16 20:11:04 -0500

    Initial commit

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <example-component></example-component>
+
+            CHARLIE MEERS
+            WEB DEVELOPER
+             816-813-0859
+             charliemeers@gmail.com  charliemeers.com
+             Lawrence, Kansas
+            Fullstack developer
+            with expertise in fast, user-centric web apps, clean maintainable code, intuitive user interfaces & automating business goals online
+            ATTRIBUTES
+            o 5 years fullstack web development experience
+            o Proven skills in PHP, HTML, CSS, JavaScript, GraphQL, Vue.js, Laravel, Node.js, Git, & SQL
+            o Firm grasp of User Experience best practices & conversion- oriented website layouts
+            o Proficient in Adobe Creative Cloud core applications
+            o Concepts and implements online solutions from design phase through development
+            & deployment
+            o Ability to multitask in fast-paced, dynamic environments with consistent, high-quality work
+            EDUCATION
+            The Evergreen State College Olympia, WA
+            Bachelor of Arts
+            Graduated May 2011
+            EXPERIENCE
+            WEB DEVELOPER @ LIFESTYLE PUBLICATIONS
+            Kansas City, MO 04/2019 – Current
+            Web Development Implement fullstack features for internal MVC Symfony/ jQuery web app from SQL queries to handling dynamic pub-sub interfaces
+            on the frontend with modular jQuery components • Integrate API data into internal app, translate between API/internal schemas, validate data • Build ElasticSearch indices for consumption by GraphQL middleware layer and node.js/Vue.js frontend • Write GraphQL and dynamoDB queries to fetch user content data originating on frontend • Implement feature requests for roots/ bedrock Wordpress multisite network • Code/maintain RESTful API service for 100+ magazine websites
+            DIRECTOR OF WEB DEVELOPMENT @ TALLGRASS STUDIOS
+            Lawrence, KS
+            10/2017 – 04/2019
+            Online Fulfillment Web App Revamp 10/2018 – Current
+            Developed & launched Laravel/jQuery web app for national healthcare company with 70+ locations • Coded custom product taxonomy with custom search engine • Debugged SQL queries to optimize site speed • Built AJAX forms, shopping cart, custom billing/shipping system, internal approval & wish list system, transactional emails & custom admin dashboard • Coded client print stationery proofing system • Coded custom frontend form validation library • Implemented & supervised staff of 5 in graphic assets production workflow for 1300+ products
+             28% Conversion rate increase  175% Reduced fulfillment labor hours
+             42% Targeted traffic increase
+             185% User Experience Score Increase
+            Design layouts/database/site navigation • Maintain/ update 30+ client & internal websites • Ensure appropriate browser/device compatibility • Integrate site functions with offline business practices • Code efficient SQL queries • Implement MVC best practices • Manage project dependencies with Composer and NPM • Version control & deploy all projects with Git • Debug front & backend errors
+            Web Ops Schedule software updates, backups & manage server resources for 20+ clients • Facilitate domain registration, transfers & maintain DNS records.
+            Online Marketing Code cross platform email templates & manage 10+ campaigns & email lists • Run SEO Audits & implement SEO plans
+            WEB DEVELOPER/DESIGNER @ TURN THE PAGE ONLINE MARKETING
+            Lee’s Summit, MO 04/2015 – 07/2016
+            Web Design/Development Designed Wordpress websites & created mockups
+            & wireframes • Pitched successful designs & site achitecture to clients in response to RFPs • Supervised a team of 5 in implementing website Coded Wordpress plugins • Engaged with personnel from 40+ companies to clarify project creative briefs • Integrated 3rd party APIs • Closed 25+ support tickets/ week
+            GRAPHIC/WEB DESIGNER @ WHITNEY DESIGN
+            Olympia, WA 05/2014 – 03/2015
+            Web/Print Design Designed Wordpress websites • Coded WP child themes & plugins • Built web assets with Adobe Creative Suite • Designed & successfully pitched logo for local youth chorus • Concepted, designed & preflighted print materials • Designed print & web ads for 60+ local companies • Managed playbill print production for 15+ local theater shows
+            Web Development
+      
+        </div>
+    </body>
+    <footer><script src='js/app.js'></script></footer>
+</html>
